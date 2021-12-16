3b30b612f0512b52fa22eaf72f9dff850d5a4056
Mohammadreza Amini <aminiamini433@yahoo.fr>
2021-12-17 01:36:16 +0330

    update gitignore

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,34 @@ import Input from "./Input";
 import { units } from "../units";
 
 function App() {
+  const [inputNum, setInputNum] = useState(0);
+  const [fromQTY, setFromQTY] = useState(1);
+  const [toQTY, setToQTY] = useState(1);
   const [result, setResult] = useState(0);
 
+  const handleFromInput = (e) => {
+    setFromQTY(e.target.value)
+  }
+  const handleToInput = (e) => {
+    setToQTY(e.target.value)
+  }
+  const handleNumInput = (e) => {
+    setInputNum(e.target.value)
+  }
+  const handleConversion = (inputNum,fromQTY,toQTY) => {
+    setResult(inputNum * fromQTY / toQTY)
+  
+  }
   return (
     <>
       <div className="converter-form">
-        <input />
+        <Input label="Amount" onChange={handleNumInput}/>
 
         <div className="row">
-          <select/>
+          <Select label="From" items={units} onChange={handleFromInput}/>
+          <Select label="To" items={units} onChange={handleToInput}/>
 
-          <button>Convert</button>
+          <button onClick={() => handleConversion(inputNum,fromQTY,toQTY)}>Convert</button>
         </div>
       </div>
 
