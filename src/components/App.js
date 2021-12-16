e995986b550732d3a0496119616929a3ca57fcda
Mohammadreza Amini <aminiamini433@yahoo.fr>
2021-12-17 00:04:01 +0330

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Select from "./Select";
+import Input from "./Input";
+import { units } from "../units";
+
+function App() {
+  const [result, setResult] = useState(0);
+
+  return (
+    <>
+      <div className="converter-form">
+        <input />
+
+        <div className="row">
+          <select/>
+
+          <button>Convert</button>
+        </div>
+      </div>
+
+      <div id="result">
+        Result is: <span data-testid="result">{result}</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
